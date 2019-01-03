21630052be694ec8215926f2f3064b72174ac849
root <root@localhost.localdomain>
2019-01-03 10:43:02 +0530

    ad report mysql_escape_string issue fix

--- src/Fa/Bundle/AdFeedBundle/Command/eazyads.sh
+++ src/Fa/Bundle/AdFeedBundle/Command/eazyads.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-/usr/bin/php -d memory_limit="512M" $DIR/../../../../../app/console fa:export-ezy-ads-feed --category="Cars"  --last_days=7
-/usr/bin/php -d memory_limit="512M" $DIR/../../../../../app/console fa:export-ezy-ads-feed --category="OnlyCars"  --last_days=7
-/usr/bin/php -d memory_limit="512M" $DIR/../../../../../app/console fa:export-ezy-ads-feed --category="Forsale"  --last_days=7
-/usr/bin/php -d memory_limit="512M" $DIR/../../../../../app/console fa:export-ezy-ads-feed --category="Gardening"  --last_days=7
-/usr/bin/php -d memory_limit="512M" $DIR/../../../../../app/console fa:export-ezy-ads-feed --category="Pets"  --last_days=7
-/usr/bin/php -d memory_limit="512M" $DIR/../../../../../app/console fa:export-ezy-ads-feed --category="Property"  --last_days=7
+/usr/bin/php -d memory_limit="512M" $DIR/../../../../../bin/console fa:export-ezy-ads-feed --category="Cars"  --last_days=7
+/usr/bin/php -d memory_limit="512M" $DIR/../../../../../bin/console fa:export-ezy-ads-feed --category="OnlyCars"  --last_days=7
+/usr/bin/php -d memory_limit="512M" $DIR/../../../../../bin/console fa:export-ezy-ads-feed --category="Forsale"  --last_days=7
+/usr/bin/php -d memory_limit="512M" $DIR/../../../../../bin/console fa:export-ezy-ads-feed --category="Gardening"  --last_days=7
+/usr/bin/php -d memory_limit="512M" $DIR/../../../../../bin/console fa:export-ezy-ads-feed --category="Pets"  --last_days=7
+/usr/bin/php -d memory_limit="512M" $DIR/../../../../../bin/console fa:export-ezy-ads-feed --category="Property"  --last_days=7
