49f01f5467266020a29f52b15597d1f7a571cffd
Rohini <rohinisubburam@bitbucket.org>
2019-07-17 15:23:44 +0530

    FFR-2855-new

--- src/Fa/Bundle/DotMailerBundle/Repository/DotmailerRepository.php
+++ src/Fa/Bundle/DotMailerBundle/Repository/DotmailerRepository.php
@@ -459,13 +459,13 @@ class DotmailerRepository extends EntityRepository
             $this->getEntityManager()->getRepository('FaDotMailerBundle:DotmailerInfo')->doTouchPointEntry($dotmailer->getId(), $adId, $touchPoint);
 
             //send to dotmailer instantly.
-            if ($isNewToDotmailer) {
+            //if ($isNewToDotmailer) {
                 exec('nohup'.' '.$container->getParameter('fa.php.path').' '.$container->getParameter('project_path').'/console fa:dotmailer:subscribe-contact --id='.$dotmailer->getId().' >/dev/null &');
                 /* $response = $this->sendOneContactToDotmailerRequest($dotmailer, $container);
                 if (isset($response['id']) && isset($response['email'])) {
                     $retresponse = $this->sendUserForDotmailerEnrollmentProgramRequest($response['id'], $container);
                 } */
-            }
+           //}
         }
     }
 
@@ -781,15 +781,15 @@ class DotmailerRepository extends EntityRepository
 
             $this->getEntityManager()->persist($dotmailer);
             $this->getEntityManager()->flush($dotmailer);
-
+            
             //send to dotmailer instantly.
-            if ($isNewToDotmailer) {
+            //if ($isNewToDotmailer) {
                 exec('nohup'.' '.$container->getParameter('fa.php.path').' '.$container->getParameter('project_path').'/console fa:dotmailer:subscribe-contact --id='.$dotmailer->getId().' >/dev/null &');
                 /* $response = $this->sendOneContactToDotmailerRequest($dotmailer, $container);
                 if (isset($response['id']) && isset($response['email'])) {
                     $retresponse = $this->sendUserForDotmailerEnrollmentProgramRequest($response['id'], $container);
                 } */
-            }
+            //}
         }
     }
 
