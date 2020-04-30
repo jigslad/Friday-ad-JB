2a1c121e40346bfbfa0f20318afed5513056da3f
Rohini <rohinisubburam@bitbucket.org>
2020-04-30 19:28:11 +0530

    adult-homepage

--- src/Fa/Bundle/EntityBundle/Repository/CategoryRepository.php
+++ src/Fa/Bundle/EntityBundle/Repository/CategoryRepository.php
@@ -690,74 +690,27 @@ class CategoryRepository extends NestedTreeRepository
      */
     public function getPostadCategoryArrayByText($term, $container = null)
     {
-        $categoryArray      = $categories = array();
+        $categoryArray      = array();
         $categoryFinalArray = array();
-        $limit              = 9;      
+        $limit              = 9;
         $term               = preg_replace('/[^A-Za-z0-9\s]/', '', trim($term));
-        $exactKeywords           = explode(' ', strtolower($term));
+        $keywords           = explode(' ', strtolower($term));
         
-        if (!empty($exactKeywords)) {
-            $excludedStopWordsKeywords = array_diff($exactKeywords, CommonManager::stopwordsList());
-            $keywords                 = array_slice($excludedStopWordsKeywords, 0, 5);
+        if (count($keywords)) {
+            $keywords                 = array_slice($keywords, 0, 5);
             $bottomLevelCategoryArray = array();
             $parentLevelCategoryArray = array();
             $keywordString            = implode('|', $keywords);
             $keywordsPattern          = '^('.$keywordString.').*$|^.*[[:space:]]('.$keywordString.').*$';
-            $exactKeywordsCnt = count($exactKeywords);
-
-            /*$categories = $this->getBaseQueryBuilder()
-                               ->andWhere("regexp(".self::ALIAS.".name, '".$keywordsPattern."') != false or regexp(".self::ALIAS.".synonyms_keywords, '".$keywordsPattern."') != false")
-                               ->andWhere(self::ALIAS.'.lvl > :level')
-                               ->setParameter('level', 1)
-                               ->orderBy(self::ALIAS.'.lvl', 'asc')
-                               ->getQuery()->getResult();*/
             
-            //Paa search keyword logic changes as per FFR-3901
-            if($exactKeywordsCnt==1) {
-                $categories = $this->getBaseQueryBuilder()->Where(self::ALIAS.".name ='".$term."'")->andWhere(self::ALIAS.'.lvl > :level')->setParameter('level', 1)->orderBy(self::ALIAS.'.id', 'asc')->getQuery()->getResult();
-                if(empty($categories)) {
-                    $categories = $this->getBaseQueryBuilder()->Where("FIND_IN_SET('".$term."',".self::ALIAS.".synonyms_keywords) > 0")->andWhere(self::ALIAS.'.lvl > :level')->setParameter('level', 1)->orderBy(self::ALIAS.'.name', 'asc')->getQuery()->getResult();
-                }                
-            } else {
-                CommonManager::$permuteresult = array();
-                CommonManager::pc_permute($exactKeywords);
-                $permutationKeywords = CommonManager::$permuteresult;
-                foreach ($permutationKeywords as $permutationKeyword) {
-                    $categories = $this->getBaseQueryBuilder()->Where(self::ALIAS.".name like '".$permutationKeyword."'")->andWhere(self::ALIAS.'.lvl > :level')->setParameter('level', 1)->orderBy(self::ALIAS.'.id', 'asc')->getQuery()->getResult();
-                    if(!empty($categories)) {
-                        break;
-                    }
-                }
-                if(empty($categories)) {
-                    foreach ($permutationKeywords as $permutationKeyword) {
-                        $categories = $this->getBaseQueryBuilder()->Where("FIND_IN_SET('".$permutationKeyword."',".self::ALIAS.".synonyms_keywords) > 0")->andWhere(self::ALIAS.'.lvl > :level')->setParameter('level', 1)->orderBy(self::ALIAS.'.name', 'asc')->getQuery()->getResult();
-                        if(!empty($categories)) {
-                            break;
-                        }
-                    }
-                }
-            }
-            if(empty($categories)) {
-                if($exactKeywordsCnt==1) {
-                    $categories = $this->getBaseQueryBuilder()->Where(self::ALIAS.".name like '".$term."%'")->andWhere(self::ALIAS.'.lvl > :level')->setParameter('level', 1)->orderBy(self::ALIAS.'.id', 'asc')->getQuery()->getResult();
-                    if(empty($categories)) {
-                        $categories = $this->getBaseQueryBuilder()->Where(self::ALIAS.".synonyms_keywords like '%".$term."%'")->andWhere(self::ALIAS.'.lvl > :level')->setParameter('level', 1)->orderBy(self::ALIAS.'.name', 'asc')->getQuery()->getResult();
-                    }
-                } else {
-                    foreach ($keywords as $keyword) {
-                        $categories = $this->getBaseQueryBuilder()->Where(self::ALIAS.".name like '".$keyword."%'")->andWhere(self::ALIAS.'.lvl > :level')->setParameter('level', 1)->orderBy(self::ALIAS.'.id', 'asc')->getQuery()->getResult();
-                        if(empty($categories)) {
-                            $categories = $this->getBaseQueryBuilder()->Where(self::ALIAS.".synonyms_keywords like '%".$keyword."%'")->andWhere(self::ALIAS.'.lvl > :level')->setParameter('level', 1)->orderBy(self::ALIAS.'.name', 'asc')->getQuery()->getResult();
-                            if(!empty($categories)) {
-                                break;
-                            }
-                        } else {
-                            break;
-                        }
-                    }
-                }
-            }
-            if (!empty($categories)) {
+            $categories = $this->getBaseQueryBuilder()
+            ->andWhere("regexp(".self::ALIAS.".name, '".$keywordsPattern."') != false or regexp(".self::ALIAS.".synonyms_keywords, '".$keywordsPattern."') != false")
+            ->andWhere(self::ALIAS.'.lvl > :level')
+            ->setParameter('level', 1)
+            ->orderBy(self::ALIAS.'.lvl', 'asc')
+            ->getQuery()->getResult();
+            
+            if (count($categories)) {
                 foreach ($categories as $category) {
                     $showTwoParentName    = false;
                     $secondRootCategoryId = $this->getSecondRootCategoryId($category->getId(), $container);
@@ -776,16 +729,16 @@ class CategoryRepository extends NestedTreeRepository
                         $bottomLevelCategoryArray = $bottomLevelCategoryArray + $selfCategoryArray;
                     }
                 }
-
+                
                 // First : check for bottom level categories
                 $bottomLevelCategoryIds = array();
                 if (count($bottomLevelCategoryArray)) {
                     $bottomLevelCategoryIds = $this->getAdCountSortedCategoryIdsByCategoryArray($bottomLevelCategoryArray, $container);
                     $bottomLevelCategoryIds = array_unique($bottomLevelCategoryIds);
-
+                    
                     // Get categories as per limit
                     $bottomLevelCategoryIds = array_slice($bottomLevelCategoryIds, 0, $limit);
-
+                    
                     $index = 1;
                     foreach ($bottomLevelCategoryIds as $categoryId) {
                         if (isset($bottomLevelCategoryArray[$categoryId])) {
@@ -795,7 +748,7 @@ class CategoryRepository extends NestedTreeRepository
                         }
                     }
                 }
-
+                
                 // Second : check for parent level categories if categories found less than limit
                 $parentLevelCategoryIds = array();
                 if (count($categoryArray) < $limit) {
@@ -803,10 +756,10 @@ class CategoryRepository extends NestedTreeRepository
                     $parentLevelCategoryIds = $this->getAdCountSortedCategoryIdsByCategoryArray($parentLevelCategoryArray, $container);
                     $parentLevelCategoryIds = array_unique($parentLevelCategoryIds);
                     $parentLevelCategoryIds = array_diff($parentLevelCategoryIds, $bottomLevelCategoryIds);
-
+                    
                     // Get parent categories as per remaining limit
                     $parentLevelCategoryIds = array_slice($parentLevelCategoryIds, 0, $remainLimit);
-
+                    
                     $index = 1;
                     foreach ($parentLevelCategoryIds as $categoryId) {
                         if (isset($parentLevelCategoryArray[$categoryId])) {
@@ -818,7 +771,7 @@ class CategoryRepository extends NestedTreeRepository
                 }
             }
         }
-
+        
         //Remove categories which is disabled to show in paa search category drop down.
         if ($categoryArray && count($categoryArray)) {
             $paaDisabledCategories = $this->getPaaDisabledCategoryIds($container);
@@ -832,7 +785,7 @@ class CategoryRepository extends NestedTreeRepository
                 $categoryFinalArray = $categoryArray;
             }
         }
-
+        
         return $categoryFinalArray;
     }
 
