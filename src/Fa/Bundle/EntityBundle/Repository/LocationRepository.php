e13332246c9f921119656ae9494b8eb785e0c176
Chaitra <chaitra.bhat@fridaymediagroup.com>
2020-11-09 18:22:54 +0530

    FFR-4693: Location area slug missing

--- src/Fa/Bundle/EntityBundle/Repository/LocationRepository.php
+++ src/Fa/Bundle/EntityBundle/Repository/LocationRepository.php
@@ -246,7 +246,7 @@ class LocationRepository extends BaseEntityRepository
         
         $areaArray = array();
         foreach ($areas as $area) {
-            $areaArray[] = array('id'=> $area->getParent()->getId(), 'text' => $area->getName().', '.$area->getParent()->getName(), 'area_id'=> $area->getId(), 'locationBy' => 'area', 'latlong' => $area->getLatitude().', '.$area->getLongitude());
+            $areaArray[] = array('id'=> $area->getParent()->getId(), 'text' => $area->getName().', '.$area->getParent()->getName(), 'area_id'=> $area->getId(), 'locationBy' => 'area', 'latlong' => $area->getLatitude().', '.$area->getLongitude(), 'slug' => $area->getUrl());
         }
         
         return $areaArray;
