be38e880e13ea3e4605b12be8fc70d6f9d3c7f4b
Rohini <rohinisubburam@bitbucket.org>
2020-12-09 00:12:03 +0530

    FFR-4693-v3

--- src/Fa/Bundle/AdBundle/Controller/AdListController.php
+++ src/Fa/Bundle/AdBundle/Controller/AdListController.php
@@ -1821,7 +1821,7 @@ class AdListController extends CoreController
                         foreach ($facetDimResult as $jsonValue => $facetCount) {
                             $town = get_object_vars(json_decode($jsonValue));
 
-                            if ($town['id'] != $searchParams['item__location'] && !in_array($locationFacetsIds, $town['id']) {
+                            if ($town['id'] != $searchParams['item__location'] && !in_array($locationFacetsIds, $town['id'])) {
                                 $locationFacetsIds[] = $town['id'];
                                 $locationFacets[] = array(
                                     'id' => $town['id'],
